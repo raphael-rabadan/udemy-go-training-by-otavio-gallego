2e8b9cf71264a372cca0972b3c054add426a922a
Raphael Rabadan <raphael.rabadan@dataprev.gov.br>
2021-03-19 13:03:46 -0300

    atualizando tudo

---
+++ 18 - Concorrência/1 - GoRoutines/made_by_me/goroutines.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	go escrever("Raphael")
+	go escrever("Laila")
+	escrever("Lara")
+
+}
+
+func escrever(texto string) {
+
+	for i := 0; ; i++ {
+
+		fmt.Printf("[%d] %s\n", i, texto)
+		time.Sleep(time.Second)
+
+	}
+	fmt.Println(" ")
+}
